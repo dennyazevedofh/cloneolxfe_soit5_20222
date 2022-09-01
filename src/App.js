218fd14284dcadff11ababfef9860e67628cc03a
Denny Paulista Azevedo Filho <denny.filho@animaeducacao.com.br>
2022-08-31 21:35:36 -0300

    Ajustando o app.css

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes'
+import { Template } from './components/MainComponents'
+import Header from './components/partials/Header'
+import Footer from './components/partials/Footer'
 
 const Page = (props) => {
   return (
     <BrowserRouter>
-      <Routes />
+      <Template>
+        <Header />
+        <Routes />
+        <Footer />
+      </Template>
     </BrowserRouter>
   )
 }
