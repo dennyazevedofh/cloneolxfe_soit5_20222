1db3f0a026e988da03b8c1c458b8c8d604174639
Denny Filho <denny@ABHNTBL2200240.anima.intranet>
2022-09-05 20:44:22 -0300

    Inciiando o Cabecalho - Logo

--- src/components/partials/Header/index.js
+++ src/components/partials/Header/index.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { HeaderArea } from './styled'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
-    return (
-        <HeaderArea>
-            ...
-        </HeaderArea>
-    )
+	return (
+		<HeaderArea>
+			<div className="container">
+				<div className="logo">
+					<Link to="/">
+						<span className="logo-1">O</span>
+						<span className="logo-2">L</span>
+						<span className="logo-3">X</span>
+					</Link>
+				</div>
+			</div>
+		</HeaderArea>
+	)
 }
 
 export default Header
