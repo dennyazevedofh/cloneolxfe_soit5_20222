e98dd4d17a697a856464072d83ce9cee16ce3dce
Denny Azevedo <denny.filho@animaeducacao.com.br>
2022-09-21 21:51:17 -0300

    Ajustes da Home - Ultimos anuncios

--- src/helpers/OlxAPI.js
+++ src/helpers/OlxAPI.js
@@ -68,6 +68,14 @@ const OlxAPI = {
 			'/categories'
 		);
 		return json.categories;
+	},
+
+	getAds: async (options) => {
+		const json = await apiFetchGet(
+			'/ad/list',
+			options
+		);
+		return json;
 	}
 }
 
--- src/pages/Home/styled.js
+++ src/pages/Home/styled.js
@@ -79,4 +79,25 @@ export const SearchArea = styled.div`
 	}
 `;
 
-export const PageArea = styled.div``;
+export const PageArea = styled.div`
+	h2 {
+		font-size: 20px;
+	}
+
+	.list {
+		display: flex;
+		flex-wrap: wrap;
+
+		.aditem {
+			width: 25%;
+		}
+	}
+
+	.seeAllLink {
+		color: #000;
+		text-decoration: none;
+		font-weight: bold;
+		display: inline-block;
+		margin-top: 10px;
+	}
+`;
