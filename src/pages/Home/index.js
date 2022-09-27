315901e2e841e003cf5972ff3e20c9790dd6ac50
Denny Filho <denny@ASP01NB001928.anima.intranet>
2022-09-26 21:50:01 -0300

    Pagina produtos - load fake

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { PageArea, SearchArea } from './styled';
-import { PageContainer, ErrorMessage } from '../../components/MainComponents';
+import { PageContainer } from '../../components/MainComponents';
 import AdItem from '../../components/partials/AdItem';
 import useApi from '../../helpers/OlxAPI';
 
